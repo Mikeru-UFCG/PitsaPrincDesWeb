2b764216b571185cd35b17b8df325db086c8d7e1
Mikeru-UFCG <miguel_macedo18@hotmail.com>
2024-10-10 21:56:58 -0300

    mudança para porta 3001

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ const { swaggerUi, specs } = require('./swagger');
 // Configuração do Swagger UI
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
   console.log(`Documentação disponível em http://localhost:${PORT}/api-docs`);
